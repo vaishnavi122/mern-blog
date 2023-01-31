039b16d23b28a7fe43dc2021e4be3320160e3a79
vaishnavi kale <kvaishnavi2312@gmail.com>
2023-01-31 17:09:17 +0530

    scriptd added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require("colors")
 db()
 const app = express()
 app.use(express.json())// middleware of json it hepls to run post method
+app.use = (express.static("public"))
 app.use(cors())
 
 app.use("/blog", require("./routes/blogRoutes"))
